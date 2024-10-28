497476c00f2a21ee2ee9e46b9ab3a7e7aa8dfe71
Dayutirta <dayumahara502@gmail.com>
2024-10-28 16:19:12 +0700

    creating a new appearance

--- resources/views/layouts/user_type/auth.blade.php
+++ resources/views/layouts/user_type/auth.blade.php
@@ -1,18 +1,9 @@
 @extends('layouts.app')
-
 @section('auth')
-
-
     @if(\Request::is('static-sign-up')) 
-        @include('layouts.navbars.guest.nav')
         @yield('content')
-        @include('layouts.footers.guest.footer')
-    
     @elseif (\Request::is('static-sign-in')) 
-        @include('layouts.navbars.guest.nav')
             @yield('content')
-        @include('layouts.footers.guest.footer')
-    
     @else
         @if (\Request::is('rtl'))  
             @include('layouts.navbars.auth.sidebar-rtl')
@@ -20,7 +11,6 @@
                 @include('layouts.navbars.auth.nav-rtl')
                 <div class="container-fluid py-4">
                     @yield('content')
-                    @include('layouts.footers.auth.footer')
                 </div>
             </main>
 
@@ -39,20 +29,16 @@
                     @yield('content')
                 </main>
             </div>
-            @include('layouts.footers.auth.footer')
-
+            
         @else
             @include('layouts.navbars.auth.sidebar')
             <main class="main-content position-relative max-height-vh-100 h-100 mt-1 border-radius-lg {{ (Request::is('rtl') ? 'overflow-hidden' : '') }}">
                 @include('layouts.navbars.auth.nav')
                 <div class="container-fluid py-4">
                     @yield('content')
-                    @include('layouts.footers.auth.footer')
                 </div>
             </main>
         @endif
-
-        @include('components.fixed-plugin')
     @endif
 
     
